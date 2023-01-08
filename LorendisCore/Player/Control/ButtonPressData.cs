153a7773774a94da0a9f23350d60e940e5ed0788
Jes <47901333+JosieJovanna@users.noreply.github.com>
2023-01-07 18:54:21 -0600

    Properties on top

--- LorendisCore/Player/Control/ButtonPressData.cs
+++ LorendisCore/Player/Control/ButtonPressData.cs
@@ -9,12 +9,30 @@ namespace LorendisCore.Player.Control
     /// </summary>
 	public struct ButtonPressData
 	{
-		public ButtonPressData(double deltaTime, bool isPressed, bool isHeld)
-		{
-			DeltaTime = deltaTime;
-			IsPressed = isPressed;
-			IsHeld = isHeld;
-		}
+		/**
+		 * Time passed in seconds since the last event happened.
+		 */
+		public double DeltaTime { get; }
+		/**
+		 * Whether the key is pressed.
+		 * Will be true if the button was just pressed or is being held.
+		 * Will be false if the button was released, or is not held.
+		 */
+		public bool IsPressed { get; }
+		/**
+		 * Whether the key is being held.
+		 * Will be true if the button is being held, or if it was just released after being held.
+		 * Will be false if the button was just pressed, or is not held.
+		 */
+		public bool IsHeld { get; }
+
+
+        public ButtonPressData(double deltaTime, bool isPressed, bool isHeld)
+        {
+            DeltaTime = deltaTime;
+            IsPressed = isPressed;
+            IsHeld = isHeld;
+        }
 
         public ButtonPressData(double deltaTime, ButtonPressType buttonPressType)
         {
@@ -23,24 +41,6 @@ namespace LorendisCore.Player.Control
             IsHeld = Util.ButtonPressTypeWasHeldLast(buttonPressType);
         }
 
-		/**
-		 * Time passed in seconds since the last event happened.
-		 */
-		public double DeltaTime { get; }
-
-		/**
-		 * Whether the key is pressed.
-		 * Will be true if the button was just pressed or is being held.
-		 * Will be false if the button was released, or is not held.
-		 */
-		public bool IsPressed { get; }
-
-		/**
-		 * Whether the key is being held.
-		 * Will be true if the button is being held, or if it was just released after being held.
-		 * Will be false if the button was just pressed, or is not held.
-		 */
-		public bool IsHeld { get; }
 
         public ButtonPressType GetButtonPressType()
         {
