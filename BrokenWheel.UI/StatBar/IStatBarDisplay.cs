a21c3e5d328543c35bfd63b3fbe700f03962c5da
Jes <JosieJovannaRice@gmail.com>
2023-07-08 23:49:17 -0500

    Added display interface for GUI

---
+++ BrokenWheel.UI/StatBar/IStatBarDisplay.cs
@@ -0,0 +1,43 @@
+﻿namespace BrokenWheel.UI.StatBar
+{
+    /// <summary>
+    /// A simple interface for controlling the regions that constitute a stat bar.
+    /// To be implemented in-engine, where the methods affect display rectangles.
+    /// Does not handle any logic regarding positioning, which is handled by <see cref="IStatBar"/>.
+    /// </summary>
+    public interface IStatBarDisplay
+    {
+        /// <summary>
+        /// Sets the stat bar's position relative to the screen, and the size of the outline.
+        /// The border is overlapped by all other display sections.
+        /// </summary>
+        void SetBarDimensions(int xPosition, int yPosition, int width, int height);
+
+        /// <summary>
+        /// Sets the stat bar's background position and size relative to the bottom-left corner of the border.
+        /// Aside from the border, the background is overlapped by all other display sections.
+        /// </summary>
+        void SetBackgroundDimensions(int xPosition, int yPosition, int width, int height);
+
+        /// <summary>
+        /// Sets the stat bar's primary-colored section's position and size relative to the bottom-left of the border.
+        /// When health is decreasing, represents the target value; when increasing, represents the current value.
+        /// Due to changing positioning, this and the secondary section should not overlap.
+        /// When stat bar is full, this will be the only section showing aside from any border.
+        /// </summary>
+        void SetPrimaryDimensions(int xPosition, int yPosition, int width, int height);
+
+        /// <summary>
+        /// Sets the stat bar's secondary-colored section's position and size relative to the bottom-left of the border.
+        /// When health is decreasing, represents the current value; when increasing, represents the target value.
+        /// Due to changing positioning, this and the primary section should not overlap.
+        /// </summary>
+        void SetSecondaryPosition(int xPosition, int yPosition, int width, int height);
+
+        /// <summary>
+        /// Sets the stat bar's exhaustion position and size relative to the bottom-left corner of the border.
+        /// The exhaustion region overlaps all other display sections.
+        /// </summary>
+        void SetExhaustionDimensions(int xPosition, int yPosition, int width, int height);
+    }
+}
---
+++ BrokenWheel.UI/StatBar/StatBar.cs
@@ -0,0 +1,30 @@
+﻿namespace BrokenWheel.UI.StatBar
+{
+    public abstract class StatBar : IStatBar
+    {
+        public void Update()
+        {
+            
+        }
+
+        public void Show()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Hide()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void IsHidden()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetDisplayMode(StatBarDisplayMode displayMode)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
