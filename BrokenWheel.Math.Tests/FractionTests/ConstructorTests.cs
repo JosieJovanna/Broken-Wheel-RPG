462dcf738368cea40bd8ec29443d72bee74bf3a9
Jes <JosieJovannaRice@gmail.com>
2023-07-16 12:51:49 -0500

    Fix constructors, add more tests, and replace double conversion

--- BrokenWheel.Math.Tests/FractionTests/ConstructorTests.cs
+++ BrokenWheel.Math.Tests/FractionTests/ConstructorTests.cs
@@ -5,7 +5,7 @@ namespace BrokenWheel.Math.Tests.FractionTests;
 
 public class ConstructorTests
 {
-    private const ZeroDenominatorOption VALID_OPTION = ZeroDenominatorOption.Ignore;
+    private const ZeroDenominatorOption IGNORE_OPTION = ZeroDenominatorOption.Ignore;
     private const int VALID_NUMBER = 3;
     
     [Fact]
@@ -19,10 +19,10 @@ public class ConstructorTests
     [Fact]
     public void PlainConstructor_Passes()
     {
-        var fraction = new Fraction(VALID_NUMBER, VALID_NUMBER, VALID_OPTION);
+        var fraction = new Fraction(VALID_NUMBER, VALID_NUMBER, IGNORE_OPTION);
         Assert.Equal(VALID_NUMBER, fraction.Numerator);
         Assert.Equal(VALID_NUMBER, fraction.Denominator);
-        Assert.Equal(VALID_OPTION, fraction.Option);
+        Assert.Equal(IGNORE_OPTION, fraction.Option);
     }
     
     [Fact]
@@ -36,10 +36,19 @@ public class ConstructorTests
     [Fact]
     public void WholeNumberConstructor_Passes()
     {
-        var fraction = new Fraction(VALID_NUMBER, VALID_OPTION);
+        var fraction = new Fraction(VALID_NUMBER, IGNORE_OPTION);
         Assert.Equal(VALID_NUMBER, fraction.Numerator);
         Assert.Equal(1, fraction.Denominator);
-        Assert.Equal(VALID_OPTION, fraction.Option);
+        Assert.Equal(IGNORE_OPTION, fraction.Option);
+    }
+
+    [Fact]
+    public void WholeNumberConstructor_Passes_WhenValueIsZero()
+    {
+        var fraction = new Fraction(0, IGNORE_OPTION);
+        Assert.Equal(0, fraction.Numerator);
+        Assert.Equal(1, fraction.Denominator);
+        Assert.Equal(IGNORE_OPTION, fraction.Option);
     }
 
     [Fact]
@@ -54,10 +63,10 @@ public class ConstructorTests
     [InlineData(.5, 1, 2)]
     public void DoubleConstructor_Passes(double toConvert, int expectedNumerator, int expectedDenominator)
     {
-        var fraction = new Fraction(toConvert, VALID_OPTION);
+        var fraction = new Fraction(toConvert, IGNORE_OPTION);
         Assert.Equal(expectedNumerator, fraction.Numerator);
         Assert.Equal(expectedDenominator, fraction.Denominator);
-        Assert.Equal(VALID_OPTION, fraction.Option);
+        Assert.Equal(IGNORE_OPTION, fraction.Option);
     }
 
     [Fact]
@@ -69,12 +78,60 @@ public class ConstructorTests
     }
 
     [Theory]
+    [InlineData("7/0", 7, 0)]
+    [InlineData("0/4", 0, 4)]
     [InlineData("1/2", 1, 2)]
-    public void StringConstructor_Passes(string toConvert, int expectedNumerator, int expectedDenominator)
+    [InlineData("-3/4", -3, 4)]
+    [InlineData("6/-3", 6, -3)]
+    [InlineData("-7/-7", -7, -7)]
+    [InlineData("019238019243/2", 019238019243, 2)]
+    [InlineData("7/2034982394", 7, 2034982394)]
+    [InlineData("7_099_821/9_000_013_232", 7_099_821, 9_000_013_232)]
+    [InlineData("68_980_001/5,473", 68_980_001, 5473)]
+    [InlineData("5,946,322,681,710/5,429,251,144,170", 5946322681710, 5429251144170)]
+    [InlineData("8___1_/,94,3455", 81, 943455)]
+    public void StringConstructor_Passes_WhenStringIsValidFractionFormat(
+        string toConvert, long expectedNumerator, long expectedDenominator)
     {
-        var fraction = new Fraction(toConvert, VALID_OPTION);
+        var fraction = new Fraction(toConvert, IGNORE_OPTION);
         Assert.Equal(expectedNumerator, fraction.Numerator);
         Assert.Equal(expectedDenominator, fraction.Denominator);
-        Assert.Equal(VALID_OPTION, fraction.Option);
+        Assert.Equal(IGNORE_OPTION, fraction.Option);
+    }
+
+    [Theory]
+    [InlineData("0.5", 1, 2)]
+    [InlineData(".3333333333333333333333333", 1, 3)]
+    [InlineData(".6666666666666666666666", 2, 3)]
+    [InlineData(".16666666666666666666666", 1, 6)]
+    [InlineData(".218614", 109307, 500000)]
+    [InlineData("809123", 809123, 1)]
+    [InlineData("1_000_000", 1_000_000, 1)]
+    [InlineData("34,000", 34000, 1)]
+    [InlineData(",,,2,,,", 2, 1)]
+    [InlineData(".0,625", 1, 16)]
+    [InlineData("._02", 1, 50)]
+    public void StringConstructor_Passes_WhenStringIsValidDecimalFormat(
+        string toConvert, long expectedNumerator, long expectedDenominator)
+    {
+        var fraction = new Fraction(toConvert, IGNORE_OPTION);
+        Assert.Equal(expectedNumerator, fraction.Numerator);
+        Assert.Equal(expectedDenominator, fraction.Denominator);
+        Assert.Equal(IGNORE_OPTION, fraction.Option);
+    }
+    
+    [Theory]
+    [InlineData("not a fraction")]
+    [InlineData("0123456789abcdef")]
+    [InlineData("1/2e")]
+    [InlineData("e1/2")]
+    [InlineData("/213")]
+    [InlineData("67/")]
+    [InlineData("21/2134/")]
+    [InlineData("123 321")]
+    [InlineData(".21301312.")]
+    public void StringConstructor_ThrowsArgumentException_WhenIncorrectlyFormatted(string toConvert)
+    {
+        Assert.Throws<FractionException>(() => new Fraction(toConvert, IGNORE_OPTION));
     }
 }
--- BrokenWheel.Math/FractionConversions.cs
+++ BrokenWheel.Math/FractionConversions.cs
@@ -1,16 +1,18 @@
 ﻿using System;
-using System.Globalization;
+using System.Linq;
 using BrokenWheel.Math.Options;
 
 namespace BrokenWheel.Math
 {
     public sealed partial class Fraction
     {
+        private const double DEFAULT_ACCURACY = 0.00000000001;
+        
         /// <summary>
         /// The function takes a floating point number as an argument 
         /// and returns its corresponding reduced fraction
         /// </summary>
-        internal static Fraction FromDouble(double value)
+        internal static Fraction FromDouble(double value, double accuracy = DEFAULT_ACCURACY)
         {
             try
             {
@@ -28,6 +30,10 @@ namespace BrokenWheel.Math
             // LOCAL FX
             Fraction FromDoubleChecked()
             {
+                if (double.IsNaN(value))
+                    throw new ArgumentException($"{nameof(value)} must be a number.");
+                if (double.IsInfinity(value))
+                    throw new ArgumentException($"{nameof(value)} must be a finite number.");
                 checked
                 {
                     return value % 1 == 0 // if whole number
@@ -37,40 +43,50 @@ namespace BrokenWheel.Math
             }
             Fraction ToFractionFromFloatingPoint()
             {
-                var tempValue = value;
-                var multiple = 1L;
-                var tempString = value.ToString(CultureInfo.InvariantCulture);
+                /* Algorithm from https://stackoverflow.com/a/32903747 */
+                if (accuracy <= 0.0 || accuracy >= 1.0)
+                    throw new ArgumentOutOfRangeException(nameof(accuracy), "Must be > 0 and < 1.");
 
-                ProcessWhole();
-                ProcessDecimal();
-                return new Fraction((int) System.Math.Round(tempValue), multiple, ZeroDenominatorOption.Ignore);
-            
-                // LOCAL FX
-                void ProcessWhole()
+                var sign = System.Math.Sign(value);
+                if (sign == -1)
+                    value = System.Math.Abs(value);
+
+                // Accuracy is the maximum relative error; convert to absolute maxError
+                var maxError = sign == 0 ? accuracy : value * accuracy;
+
+                var n = (int) System.Math.Floor(value);
+                value -= n;
+
+                if (value < maxError)
+                    return new Fraction(sign * n, 1);
+                if (1 - maxError < value)
+                    return new Fraction(sign * (n + 1), 1);
+
+                var lowerNumerator = 0;
+                var lowerDenominator = 1;
+
+                var upperNumerator = 1;
+                var uppderDenominator = 1;
+
+                while (true)
                 {
-                    while (tempString.IndexOf("E", StringComparison.Ordinal) > 0) // if in the form like 12E-9
+                    var MiddleNumerator = lowerNumerator + upperNumerator;
+                    var middleDenominator = lowerDenominator + uppderDenominator;
+
+                    if (middleDenominator * (value + maxError) < MiddleNumerator)
                     {
-                        tempValue *= 10;
-                        multiple *= 10;
-                        tempString = tempValue.ToString(CultureInfo.InvariantCulture);
+                        upperNumerator = MiddleNumerator;
+                        uppderDenominator = middleDenominator;
                     }
-                }
-                void ProcessDecimal()
-                {
-                    var digitsAfterDecimal = tempString.Length - IndexOfDecimalPoint() - 1;
-                    while (digitsAfterDecimal > 0)
+                    else if (MiddleNumerator < (value - maxError) * middleDenominator)
+                    { 
+                        lowerNumerator = MiddleNumerator;
+                        lowerDenominator = middleDenominator;
+                    }
+                    else // middle is best fraction
                     {
-                        tempValue *= 10;
-                        multiple *= 10;
-                        digitsAfterDecimal--;
+                        return new Fraction((n * middleDenominator + MiddleNumerator) * sign, middleDenominator);
                     }
-                }
-                int IndexOfDecimalPoint()
-                {
-                    var index = 0;
-                    while (tempString[index] != '.')
-                        index++;
-                    return index;
                 }
             }
         }
@@ -83,13 +99,20 @@ namespace BrokenWheel.Math
         internal static Fraction FromString(string value)
         {
             var i = IndexOfSlash();
-            if (i == value.Length) // if string is not in the form of a fraction
-                return Convert.ToDouble(value);
-            
-            var numerator = Convert.ToInt64(value.Substring(0, i));
-            var denominator = Convert.ToInt64(value.Substring(i + 1));
-            return new Fraction(numerator, denominator);
-            
+            try
+            {
+                CheckStringFormat();
+                return ConvertFromString();
+            }
+            catch (FractionException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new FractionException($"Failed to convert from string '{value}'.", ex);
+            }
+
             // LOCAL FX
             int IndexOfSlash()
             {
@@ -98,6 +121,38 @@ namespace BrokenWheel.Math
                     if (value[j] == '/')
                         break;
                 return j;
+            }
+            void CheckStringFormat()
+            {
+                if (i == 0 || i == value.Length - 1)
+                    throw new FractionException(
+                        $"Could not convert string '{value}' - slash cannot be first or last character.");
+                if (value.Count(_ => _ == '/') > 1)
+                    throw new FractionException(
+                        $"Could not convert string '{value}' - cannot have more than one slash.");
+
+            }
+            Fraction ConvertFromString()
+            {
+                if (i == value.Length) // if string is not in the form of a fraction
+                    return FromDouble(Convert.ToDouble(SanitizeString(value)));
+
+                var numerator = ConvertSubStringToLong(0, i);
+                var denominator = ConvertSubStringToLong(i + 1);
+                return new Fraction(numerator, denominator, ZeroDenominatorOption.Ignore);
+            }
+            long ConvertSubStringToLong(int startIndex, int? length = null)
+            {
+                var substring = length == null 
+                    ? value.Substring(startIndex) 
+                    : value.Substring(startIndex, length.Value);
+                return Convert.ToInt64(SanitizeString(substring));
+            }
+            string SanitizeString(string toSanitize)
+            {
+                return toSanitize
+                    .Replace("_", "")
+                    .Replace(",", "");
             }
         }
     }
