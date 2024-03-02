f48f7a34c39a059a3d8d021139cbff03d94e02ba
Jes <47901333+JosieJovanna@users.noreply.github.com>
2024-03-01 21:26:39 -0600

    Add more jumping-related meta and marionette properties

--- BrokenWheel.Control/Puppeteering/IMarionette.cs
+++ BrokenWheel.Control/Puppeteering/IMarionette.cs
@@ -7,10 +7,29 @@ namespace BrokenWheel.Control.Puppeteering
     /// </summary>
     public interface IMarionette : IPuppet
     {
+        /// <summary>
+        /// Whether this entity is on the ground (beneath it, relatively).
+        /// </summary>
         bool IsOnGround { get; }
 
+        /// <summary>
+        /// How long, in seconds, this entity has been in the air.
+        /// </summary>
+        double TimeInAir { get; }
+
+        /// <summary>
+        /// The number of jumps performed by this entity since last touching the ground.
+        /// </summary>
+        int NumberOfJumps { get; }
+
+        /// <summary>
+        /// Make this entity jump, with the specified strength.
+        /// </summary>
         void Jump(float strength);
 
+        /// <summary>
+        /// Sets the modes of movement for this entity.
+        /// </summary>
         void SetMovementState(MovementStance stance, MovementSpeed speed);
 
         // TODO: action handling
