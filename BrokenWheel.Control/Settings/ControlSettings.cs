a47a75d134560b49d079f9d4df01c1b394ba6957
Jes <47901333+JosieJovanna@users.noreply.github.com>
2024-03-08 23:30:03 -0600

    Add more settings and meta for height and camera

--- BrokenWheel.Control/Settings/ControlSettings.cs
+++ BrokenWheel.Control/Settings/ControlSettings.cs
@@ -12,12 +12,13 @@ namespace BrokenWheel.Control.Settings
 
         public float CameraPanSensitivity { get; set; } = 1.7f;
         public float CameraTiltSensitivity { get; set; } = 1.5f;
+        public float CameraHeightOffset { get; set; } = -0.05f;
 
 
         public bool InvertLookVertical { get; set; } = false;
         public float LowestCameraTilt { get; set; } = AngleUtil.DegreesToRadians(-85);
         public float HighestCameraTilt { get; set; } = AngleUtil.DegreesToRadians(90);
 
-        public float CrawlHoldTime { get; set; } = 1.0f;
+        public float CrawlHoldTime { get; set; } = 0.85f;
     }
 }
--- BrokenWheel.Entity/Meta/EntityMeta.cs
+++ BrokenWheel.Entity/Meta/EntityMeta.cs
@@ -4,6 +4,10 @@ namespace BrokenWheel.Entity.Meta
 {
     public class EntityMeta
     {
+        public float Height { get; set; } = 1.75f;
+        public float CrouchHeightFactor { get; set; } = 0.5f;
+        public float CrawlHeightFactor { get; set; } = 0.25f;
+
         public PhysicsMeta Physics { get; set; } = new PhysicsMeta();
 
         public MovementMeta Movement { get; set; } = new MovementMeta();
