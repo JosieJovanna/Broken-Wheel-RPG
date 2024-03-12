187b74f09aaea826c20f877f3de2d9f4b23ddda3
Jes <47901333+JosieJovanna@users.noreply.github.com>
2024-03-11 20:47:22 -0500

    Recursive settings registry

--- BrokenWheel.Core/DependencyInjection/Implementation/SettingsModule.cs
+++ BrokenWheel.Core/DependencyInjection/Implementation/SettingsModule.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using BrokenWheel.Core.Logging;
 using BrokenWheel.Core.Settings;
 
@@ -23,10 +25,36 @@ namespace BrokenWheel.Core.DependencyInjection.Implementation
         {
             var type = typeof(TSettings);
             _logger.LogCategory(LogCategory.DEPENDENCY_INJECTION, $"Creating default {type.Name} as there is no existing settings file...");
-            var constructor = type.GetConstructor(Type.EmptyTypes);
-            var settings = Cast<TSettings>(constructor.Invoke(null));
-            RegisterSettings(settings);
+            var settings = ConstructSettings<TSettings>();
+            RegisterSettings(settings); // TODO: recursively find children settings that are registered
             return settings;
+        }
+
+        private TSettings ConstructSettings<TSettings>() where TSettings : class, ISettings
+        {
+            var type = typeof(TSettings);
+            var constructor = type.GetConstructor(Type.EmptyTypes);
+            var instance = constructor.Invoke(null);
+            return Cast<TSettings>(instance);
+        }
+
+        private void SetRegisteredChildSettings(ISettings settings)
+        {
+            foreach (var prop in GetSubSettingProperties(settings))
+                if (_settingsRegistry.ContainsKey(prop.PropertyType))
+                    TrySettingRegisteredSubSettingProperty(settings, prop);
+        }
+
+        private void TrySettingRegisteredSubSettingProperty(ISettings settings, PropertyInfo prop)
+        {
+            try
+            {
+                prop.SetValue(settings, _settingsRegistry[prop.PropertyType]);
+            }
+            catch (Exception e)
+            {
+                throw new DependencyException($"Exception ocurred while trying to set {settings.GetType().Name}.{prop.Name} to registered {prop.PropertyType.Name} value - {e.Message}", e);
+            }
         }
 
         /// <inheritdoc/>
@@ -36,10 +64,31 @@ namespace BrokenWheel.Core.DependencyInjection.Implementation
             if (_settingsRegistry.ContainsKey(type))
                 LogAndThrow($"Settings `{type.Name}` already registered.");
 
-            _settingsRegistry.Add(type, settings);
-            _logger.LogCategory(LogCategory.DEPENDENCY_INJECTION, $"Registered `{typeof(TSettings).FullName}`.");
-            // TODO: recursively register (if not already registered) any child settings AND if the settings is already registered when instantiating, then set those child properties to the initialized settings.
+            Register(type, settings);
             return this;
+        }
+
+        private void Register(Type type, ISettings settings)
+        {
+            _settingsRegistry.Add(type, settings);
+            _logger.LogCategory(LogCategory.DEPENDENCY_INJECTION, $"Registered `{type.FullName}`.");
+            RecursiveRegister(settings);
+        }
+
+        private void RecursiveRegister(ISettings settings)
+        {
+            foreach (var prop in GetSubSettingProperties(settings))
+                if (!_settingsRegistry.ContainsKey(prop.PropertyType))
+                    Register(prop.PropertyType, settings);
+        }
+
+        private IEnumerable<PropertyInfo> GetSubSettingProperties(ISettings settings)
+        {
+            var iSettings = typeof(ISettings);
+            return settings.GetType().GetProperties()
+                .Where(_ => _.PropertyType
+                    .GetInterfaces()
+                    .Contains(iSettings));
         }
     }
 }
